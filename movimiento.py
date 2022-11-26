4694d4de084a0869e6bd7dc810325c9ba1fbb8e2
mabustillo14 <mabustillo02@gmail.com>
2022-11-26 12:31:23 -0300

    Movimiento sin comentarios extra

--- movimiento.py
+++ movimiento.py
@@ -3,39 +3,24 @@ global Intrucciones
 Instrucciones = []
 
 def Apilar(x,y): #Apilar un bloque sobre otro
-    #print("Apilar(", x,",", y, ")")
-    #print("\tPRECOND: Libre(",y,"), Sujetar(",x,")")
-    #print("\tEFECTO: Brazo_Libre, Sobre(", x ,",", y ,"), Libre(", x ,")")
     global Intrucciones
     Instrucciones.append("Apilar("+ x +"," + y + ")")
     Instrucciones.append("\tPRECOND: Libre(" + y + "), Sujetar(" + x + ")")
     Instrucciones.append("\tEFECTO: Brazo_Libre, Sobre(" + x + "," + y +"), Libre("+ x +")")
 
 def Desapilar(x,y):#Quitar un bloque que estaba sobre otro
-    #print("Desapilar(", x,",", y, ")")
-    #print("\tPRECOND: Sobre(",x,",",y,") , Libre(",x,"), Brazo_Libre")
-    #print("\tEFECTO: Sujetar (", x ,"), Libre (", y ,")")
-
     global Intrucciones
     Instrucciones.append("Desapilar("+ x +"," + y + ")")
     Instrucciones.append("\tPRECOND: Sobre(" + x + "," + y + ") , Libre(" + x + "), Brazo_Libre")
     Instrucciones.append("\tEFECTO: Sujetar (" + x + "), Libre (" + y + ")")
 
 def Sujetar(x): #Sujetar un bloque de la Mesa
-    #print("Sujetar(",x,")")
-    #print("\tPRECOND: Libre(",x,") , En_Mesa(",x,"), Brazo_Libre")
-    #print("\tEFECTO: Sujetar (", x ,")")
-
     global Intrucciones
     Instrucciones.append("Sujetar(" + x + ")")
     Instrucciones.append("\tPRECOND: Libre("+ x + ") , En_Mesa(" + x + "), Brazo_Libre")
     Instrucciones.append("\tEFECTO: Sujetar (" + x + ")")
 
 def Soltar(x): #Soltar un bloque en la Mesa
-    #print("Soltar(",x,")")
-    #print("\tPRECOND: Sujetar(",x,")")
-    #print("\tEFECTO: En_Mesa (", x ,"), Brazo_Libre, Libre(",x,")")
-    
     global Intrucciones
     Instrucciones.append("Soltar(" + x + ")")
     Instrucciones.append("\tPRECOND: Sujetar(" + x + ")")
@@ -43,13 +28,12 @@ def Soltar(x): #Soltar un bloque en la Mesa
 
 
 
-
 if __name__ == '__main__':
     print('--------------------Lenguaje STRIPS--------------------')
     Posicion_Inicial = ['R1','R2','R3','R4']
     Posicion_Final = ['R3','R1','R4','R2']
-    print("Posicion Inicial ", Posicion_Inicial)
-    print("Posicion Final ", Posicion_Final)
+    #print("Posicion Inicial ", Posicion_Inicial)
+    #print("Posicion Final ", Posicion_Final)
     Orden = [Posicion_Inicial[0]]
     Plan = []
 
@@ -94,18 +78,14 @@ if __name__ == '__main__':
         
         i+=1
     
-    print("\nSolucion para el Usuario\n")
-    
+    #Mostrar Solucionn para el Usuario
+    print("\nSolucion para el Usuario\n")    
     for i  in range(len(Plan)):
         print(Plan[i])
     
-
-
+    #Mostrar Solucion en lenguaje STRIPS
     print("\nInstrucciones en formato Strips\n")
-    
     for i  in range(len(Instrucciones)):
         print(Instrucciones[i])
     
     print("\nFin de la Ejecución")
-
-
