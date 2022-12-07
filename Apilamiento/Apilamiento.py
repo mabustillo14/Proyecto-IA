086066c80259e6abbf15708f58be2bd70e7a4986
mabustillo14 <mabustillo02@gmail.com>
2022-12-07 11:04:28 -0300

    Se puede usar sin interfaz grafica

--- Apilamiento/Apilamiento.py
+++ Apilamiento/Apilamiento.py
@@ -1,90 +1,127 @@
-#Variable Global de Instrucciones en formato STRIPS
-global Intrucciones
-Instrucciones = []
-
 def Apilar(x,y): #Apilar un bloque sobre otro
-    global Intrucciones
-    Instrucciones.append("Apilar("+ x +"," + y + ")")
-    Instrucciones.append("\tPRECOND: Libre(" + y + "), Sujetar(" + x + ")")
-    Instrucciones.append("\tEFECTO: Brazo_Libre, Sobre(" + x + "," + y +"), Libre("+ x +")")
+    print("\tApilar("+ x +"," + y + ")")
+    print("\t\tPRECOND: Libre(" + y + "), Sujetar(" + x + "), Caja("+ x + "), Caja(" + y + ") (" + x + " ≠ " + y +")")
+    print("\t\tEFECTO: Libre(Brazo), Sobre(" + x + "," + y +"), Libre("+ x +"), ~Libre("+ y +")")
 
 def Desapilar(x,y):#Quitar un bloque que estaba sobre otro
-    global Intrucciones
-    Instrucciones.append("Desapilar("+ x +"," + y + ")")
-    Instrucciones.append("\tPRECOND: Sobre(" + x + "," + y + ") , Libre(" + x + "), Brazo_Libre")
-    Instrucciones.append("\tEFECTO: Sujetar (" + x + "), Libre (" + y + ")")
+    print("\tDesapilar("+ x +"," + y + ")")
+    print("\t\tPRECOND: Sobre(" + x + "," + y + ") , Caja("+ x + "), Caja(" + y + "), Libre(" + x + "), Libre(Brazo), (" + x + " ≠ " + y +")")
+    print("\t\tEFECTO: Sujetar (" + x + "), Libre (" + y + "), ~Sobre(" + x + "," + y + ") ~Libre(Brazo)" )
 
 def Sujetar(x): #Sujetar un bloque de la Mesa
-    global Intrucciones
-    Instrucciones.append("Sujetar(" + x + ")")
-    Instrucciones.append("\tPRECOND: Libre("+ x + ") , En_Mesa(" + x + "), Brazo_Libre")
-    Instrucciones.append("\tEFECTO: Sujetar (" + x + ")")
+    print("\tSujetar(" + x + ")")
+    print("\t\tPRECOND: Libre("+ x + "), Libre(Brazo), Caja("+ x + ")")
+    print("\t\tEFECTO: Sujetar (" + x + "), ~Libre(Brazo)")
 
-def Soltar(x): #Soltar un bloque en la Mesa
-    global Intrucciones
-    Instrucciones.append("Soltar(" + x + ")")
-    Instrucciones.append("\tPRECOND: Sujetar(" + x + ")")
-    Instrucciones.append("\tEFECTO: En_Mesa (" + x + "), Brazo_Libre, Libre(" + x + ")")
-
-def Solucion(Posicion_Inicial, Posicion_Final): #Secuencia de pasos a seguir
-    Orden = [Posicion_Inicial[0]]
-    Plan = []
-    global Intrucciones
-    
-    #Añadir las Condiciones del Problema
-    inicio = ""
-    objetivo =  ""
-    for i  in range(len(Posicion_Inicial)-1):
-        #Anadir Condiciones Iniciales
-        inicio +="(" + Posicion_Inicial[i+1] + " sobre " + Posicion_Inicial[i] + "), "
-        #Anadir el Objetivo del problema
-        objetivo += "(" + Posicion_Final[i+1] + " sobre " + Posicion_Final[i] + "), "
-    Instrucciones.append("Inicio: " + inicio)
-    Instrucciones.append("Objetivo: " + objetivo + "\n")
-
-    for i in range(len(Posicion_Inicial)-1):
-        mover_pieza = Posicion_Inicial[len(Posicion_Inicial)-1-i]
-        pieza_apoyada = Posicion_Inicial[len(Posicion_Inicial)-2-i]
-        Desapilar(mover_pieza, pieza_apoyada)
-        Soltar(mover_pieza)
-        Plan.append("Poner " + mover_pieza + " sobre la Mesa ")
-        Orden.append(mover_pieza)
-
-    #Secuencia de Apilamiento para obtener la posicion final
-    flag = True
-    i=1 #Porque la base del nuevo apilado esta sobre la mesa
-    while (flag):
-        if(i == len(Posicion_Final)-1):
-            flag = False
-
-        for j in range(len(Orden)):
-            if(Posicion_Final[i]==Orden[j]):
-                Sujetar(Orden[j])
-                Apilar(Orden[j], Posicion_Final[i-1])
-                Plan.append("Poner " + Orden[j] + " sobre " + Posicion_Final[i-1])
-        i+=1      
-
-    #Mostrar Solucionn para el Usuario
-    print("\nSolucion para el Usuario\n")    
-    for i  in range(len(Plan)):
-        print(Plan[i])
-    
-    #Mostrar Solucion en lenguaje STRIPS
-    print("\nInstrucciones en formato Strips\n")
-    for i  in range(len(Instrucciones)):
-        print(Instrucciones[i])
-    
+def Soltar(x,mesa): #Soltar un bloque en la Mesa
+    print("\tSoltar(" + x + ","+ mesa +")")
+    print("\t\tPRECOND: Sujetar(" + x + "), ~Libre(Brazo), Caja("+ x + ")")
+    print("\t\tEFECTO: En_Mesa (" + x + ","+ mesa +"), ~Sujetar(" + x + "), Libre(" + x + "), Libre(Brazo)" )    
 
 def Mostrar_Apilado(Apilado):
+    print("Representación Simbolica del Resultado de la secuencia:")
     for i in range(len(Apilado)):
-        print("-------------------------")
-        print("|\t", Apilado[len(Apilado)-1-i]," \t|")
-    print("=========================\n")
+        print("\t-------------------------")
+        print("\t|\t", Apilado[len(Apilado)-1-i]," \t|")
+    print("\t=========================\n")
 
 
-if __name__ == '__main__':
-    print('--------------------Lenguaje STRIPS--------------------')
-  
+def Solucion(Posicion_Inicial,Caja_1,Caja_2,Caja_3,Caja_4): #Secuencia de pasos a seguir
+    secuencia_objetivo = [Caja_4,Caja_3,Caja_2,Caja_1] #Posicion Final
+    mesas = ["mesa A","mesa B", "mesa C", "mesa D"]
+
+    #Validamos los datos ingresados
+    #Convertimos la lista en un ser donde no toma elementos repetidos
+    if(len(secuencia_objetivo) == len(set(secuencia_objetivo))):#La secuencia tiene la misma cantidad de componentes de la posicion inicial y no se repite ninguna
+       
+        #Auxiliares
+        Orden = [] #Como estan apoyadas en la mesa
+        pie = Posicion_Inicial.index(Caja_4)#La mesa sobre donde esta el inicio de la nueva torre
+
+        #Añadir las Condiciones del Problema
+        inicio = "Sobre(" +Posicion_Inicial[0] + "," +  mesas[0]+"), "
+        objetivo = "Sobre(" +secuencia_objetivo[0] + "," +  mesas[pie]+"), "
+        objetos = "Caja("+Posicion_Inicial[0] +"), "
+        superficies = mesas[0] +", "
+
+        #Salida: Secuencia Solución y Costo 
+        secuencia = ""
+        costo = 0
+        
+        for i  in range(len(Posicion_Inicial)-1):
+            #Anadir Condiciones Iniciales
+            #inicio +="(" + Posicion_Inicial[i+1] + " sobre " + Posicion_Inicial[i] + "), "
+            inicio += "Sobre(" + Posicion_Inicial[i+1] + "," + Posicion_Inicial[i] + "), "
+            #Anadir el Objetivo del problema
+            objetivo += "Sobre(" + secuencia_objetivo[i+1] + "," + secuencia_objetivo[i] + "), "
+            objetos += "Caja("+Posicion_Inicial[i+1] +"), "
+            superficies += mesas[i+1] + ", "
+        objetos += superficies + " Brazo"
+        inicio += "Libre(" + secuencia_objetivo[len(secuencia_objetivo)-1] + "), Libre(Brazo)"
+
+        #Mostrar por pantalla Condiciones Iniciales
+        print("\n--------------------------------------------------------------------")
+        print("PROPIEDADES:")
+        print("\tObjetos: " + objetos )
+        print("\tInicio: " + inicio)
+        print("\tObjetivo: " + objetivo)
+
+        #Mostrar las Funciones disponibles
+        print("\n--------------------------------------------------------------------")
+        print("ACCIONES:")
+        Desapilar("x","y")
+        Sujetar("x")
+        Soltar("x", "mesa")
+        Apilar("x", "y")
+
+        #Poner todas las cajas sobre la mesa
+        for i in range(len(Posicion_Inicial)-1):
+            mover_pieza = Posicion_Inicial[len(Posicion_Inicial)-1-i]
+            pieza_apoyada = Posicion_Inicial[len(Posicion_Inicial)-2-i]
+            secuencia += "Desapilar(" + mover_pieza + "," + pieza_apoyada+"), "
+            costo +=1
+            #Desapilar(mover_pieza, pieza_apoyada)
+            secuencia += "Sujetar(" + mover_pieza + "), "
+            costo +=1
+            secuencia += "Soltar(" + mover_pieza + ","+ mesas[len(mesas)-i-1]+"), "
+            costo +=1
+            Orden.append(Posicion_Inicial[i])
+            
+        #Secuencia de Apilamiento para obtener la posicion final
+        flag = True
+        i=1 #Porque la base del nuevo apilado esta sobre la mesa
+        while (flag):
+            if(i == len(secuencia_objetivo)-1):
+                flag = False
+
+            for j in range(len(Orden)):
+                if(secuencia_objetivo[i]==Orden[j]):
+                    secuencia += "Sujetar(" + Orden[j] + "), "
+                    costo +=1
+                    #Sujetar(Orden[j])
+                    
+                    secuencia += "Apilar(" + Orden[j] + "," + secuencia_objetivo[i-1] + "), "
+                    costo +=1
+                    #Apilar(Orden[j], Posicion_Final[i-1])
+                    ##Plan.append("Poner " + Orden[j] + " sobre " + Posicion_Final[i-1])
+            i+=1
+
+        print("\n--------------------------------------------------------------------")
+        print("OUTPUTS: ")
+        print("Secuencia Solución: ")
+        print(secuencia)
+        print("\tCantidad de Movimientos: " + str(costo) + " Movimientos"  )
+        
+        print("\n--------------------------------------------------------------------")
+        Mostrar_Apilado(secuencia_objetivo)
+           
+        return objetos, inicio, objetivo, secuencia, str(costo) + " Movimientos"
+    else:
+        return ("Error - Intente de nuevo ingresar un orden de apilamiento valido\n"), None, None, None, None
+
+
+
+if __name__ == '__main__': #Para que se pueda usar sin interfaz
     #Extraemos los datos del txt que contiene el orden de apilamiento con vision artificial
     with open("datos.txt") as archivo:
         for linea in archivo:
@@ -113,21 +150,12 @@ if __name__ == '__main__':
     Posicion_Final = []
     for i in range(len(secuencia_objetivo)):
         Posicion_Final.append(Posicion_Inicial[int(secuencia_objetivo[i])])
-    
     print("\nApilamiento ascendente Objetivo: ", Posicion_Final)
    
-    #Posicion_Inicial = ['R1','R2','R3','R4','R5']
-    #Posicion_Final = ['R3','R1','R4','R5','R2']
-    #print("Posicion Inicial ", Posicion_Inicial)
-    #print("Posicion Final ", Posicion_Final)
-
     #Ejecutar la funcion para obtener la solucion
-    Solucion(Posicion_Inicial, Posicion_Final)
-
-    print("\nRepresentación Gráfica de la Posición Inicial de Apilado")
-    Mostrar_Apilado(Posicion_Inicial)
-
-    print("\nRepresentación Gráfica de la Posición Final de Apilado")
-    Mostrar_Apilado(Posicion_Final)
+    Solucion(Posicion_Inicial, Posicion_Final[3],Posicion_Final[2],Posicion_Final[1], Posicion_Final[0])
 
     print("\nFin de la Ejecución")
+
+
+
---
+++ Apilamiento/gui_apilamiento.py
@@ -0,0 +1,49 @@
+"""
+GUI: Graphical User Interface
+Hacemos uso de Gradio: pip3 install gradio
+"""
+
+import gradio as gr
+from Apilamiento import Solucion
+
+def main(Caja_1, Caja_2, Caja_3, Caja_4):
+    return Solucion(Posicion_Inicial,Caja_1, Caja_2, Caja_3, Caja_4)
+
+# Gradio Format
+title = "Apilamiento 🚀"
+description = 'Input: Orden de Apilamiento Objetivo. <br> Output: Propiedades del Problema, Secuencia en Lenguaje STRIPS , Cantidad de Movimientos'
+article="**Mario Bustillo 2022** | [Github](https://github.com/mabustillo14) | [Linkedin](https://www.linkedin.com/in/mario-bustillo/) 🤗"
+enable_queue=False
+
+#Salida de datos
+text1 = gr.Textbox(label="Objetos")
+text2 = gr.Textbox(label="Estado Inicial")
+text3 = gr.Textbox(label="Estado Objetivo")
+text4 = gr.Textbox(label="Secuencia Solución")
+text5 = gr.Textbox(label="Costo total del camino")
+
+#Extraer los objetos 
+with open("datos.txt") as archivo:
+        for linea in archivo:
+            Posicion_Inicial = linea.split(",")
+            Posicion_Inicial.pop()
+        print("\nApilamiento ascendente Inicial: ", Posicion_Inicial)
+
+inputs =  []
+for i in range(4):
+    titulo = "Caja " +  str(4-i)
+    inputs.append(gr.Radio(Posicion_Inicial))
+
+#demo = gr.Interface(fn=main, inputs=[text1, text2, text3, text4], outputs=[text5,image1],title=title,description=description,article=article, css="body {background-image: url('file=mapa.png')}")
+demo = gr.Interface(
+    main,
+    inputs,
+    outputs=[text1, text2, text3, text4, text5],
+    title=title,
+    description=description,
+    article=article
+)
+
+
+
+demo.launch(enable_queue=enable_queue,debug=True)
