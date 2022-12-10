5c0c490bbe3a3d33e6312e8f6d2e245188171b99
cabustillo13 <cabustillo13@hotmail.com>
2022-12-10 15:33:30 -0300

    [UPDATE] renombrar archivos que comiencen con p

--- Clasificador-de-imagenes/gui_clasificacion.py
+++ Clasificador-de-imagenes/gui_clasificacion.py
@@ -4,31 +4,29 @@ Hacemos uso de Gradio: pip3 install gradio
 """
 
 import gradio as gr
-from Clasificacion import main
+from p7_clasificacion import main
 
 # Gradio Format
 title = "Clasificación 🚀"
 description = 'Input: Fotos de las piezas según como estan apiladas. <br> Output: Orden de Apilamiento'
-article="**Mario Bustillo 2022** | [Github](https://github.com/mabustillo14) | [Linkedin](https://www.linkedin.com/in/mario-bustillo/) 🤗"
-enable_queue=False
+article = "**Mario Bustillo 2022** | [Github](https://github.com/mabustillo14) | [Linkedin](https://www.linkedin.com/in/mario-bustillo/) 🤗"
+enable_queue = False
 
-#Entrada de datos
+# Entrada de datos
 image1 = gr.Image(shape=(224, 224), label="Foto - Caja 1")
 image2 = gr.Image(shape=(224, 224), label="Foto - Caja 2")
 image3 = gr.Image(shape=(224, 224), label="Foto - Caja 3")
 image4 = gr.Image(shape=(224, 224), label="Foto - Caja 4")
 
-
-
-#Salida de datos
+# Salida de datos
 text1 = gr.Textbox(label="Resultado del Análisis - Caja 1")
 text2 = gr.Textbox(label="Resultado del Análisis - Caja 2")
 text3 = gr.Textbox(label="Resultado del Análisis - Caja 3")
 text4 = gr.Textbox(label="Resultado del Análisis - Caja 4")
 text5 = gr.Textbox(label="Orden ascendente de Apilamiento")
 
-#Armar la GUI
-demo = gr.Interface(fn=main, inputs=[image4,image3,image2,image1] , outputs=[text4, text3,text2,text1,text5],title=title,description=description,article=article)
+# Armar la GUI
+demo = gr.Interface(fn=main, inputs=[image4, image3, image2, image1], outputs=[text4, text3, text2, text1, text5],
+                    title=title, description=description, article=article)
 
-demo.launch(enable_queue=enable_queue,debug=True)
-
+demo.launch(enable_queue=enable_queue, debug=True)
